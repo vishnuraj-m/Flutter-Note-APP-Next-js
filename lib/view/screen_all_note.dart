93ee35e8069884f78e7b442ffd4acad8e4f7e258
vishnuraj-m <acappella.studiov@gmail.com>
2022-10-03 11:08:47 +0530

    ui section completed

---
+++ lib/view/screen_all_note.dart
@@ -0,0 +1,117 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:note_app_sample/view/screen_add_note.dart';
+
+class ScreenAllNotes extends StatelessWidget {
+  const ScreenAllNotes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('All Notes'),
+      ),
+      body: SafeArea(
+        child: GridView.count(
+          crossAxisCount: 2,
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+          padding: const EdgeInsets.all(20),
+          children: List.generate(
+              10,
+              (index) => NoteItem(
+                    id: index.toString(),
+                    title: 'Lorem Ipsum $index',
+                    content:
+                        't is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+                  )),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (ctx) => ScreenAddNote(
+                type: ActionType.addNote,
+              ),
+            ),
+          );
+        },
+        label: const Text('New'),
+        icon: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class NoteItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String content;
+  const NoteItem({
+    super.key,
+    required this.id,
+    required this.title,
+    required this.content,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (ctx) => ScreenAddNote(
+              type: ActionType.editNote,
+              id: id,
+            ),
+          ),
+        );
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(color: Colors.grey),
+        ),
+        padding: const EdgeInsets.all(5),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Text(
+                    title,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.delete,
+                    color: Colors.red,
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              content,
+              maxLines: 5,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                color: Colors.black,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
